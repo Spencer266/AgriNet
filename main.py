bec0fa5c6ff72f853564730bdc5ec059f01e6503
Hoangdao192 <nguyendanghoangdao2002@gmail.com>
2022-04-20 08:52:36 +0700

    fix image error, delete unuse, refactor

--- main.py
+++ main.py
@@ -8,9 +8,34 @@ def home():
     return render_template('test.html')
 
 
-@app.route("/detail")
+@app.route("/index.html")
+def home2():
+    return render_template('test.html')
+
+
+@app.route("/shop-details")
 def details():
     return render_template('shop-details.html')
+
+
+@app.route("/shop-grid")
+def shop_grid():
+    return render_template('shop-grid.html')
+
+
+@app.route("/shopping-cart")
+def shop_cart():
+    return render_template('shoping-cart.html')
+
+
+@app.route("/checkout")
+def checkout():
+    return render_template('checkout.html')
+
+
+@app.route("/contact")
+def contact():
+    return render_template('contact.html')
 
 
 if __name__ == '__main__':
