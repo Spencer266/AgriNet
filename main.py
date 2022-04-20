6921cd93e2e50b11373cc3a09c9d946e109c2a9d
Hoangdao192 <nguyendanghoangdao2002@gmail.com>
2022-04-20 13:50:49 +0700

    registration page

--- main.py
+++ main.py
@@ -43,6 +43,11 @@ def login():
     return render_template('login.html')
 
 
+@app.route("/registration")
+def registration():
+    return render_template('registration.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
 
